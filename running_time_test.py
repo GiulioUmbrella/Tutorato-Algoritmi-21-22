f516784fe732855c64141029510a732e10a26d04
Giulio <giulio.umbrella@gmail.com>
2021-10-14 14:33:49 +0200

    Add file for time complexity evaluation

--- running_time_test.py
+++ running_time_test.py
@@ -33,30 +33,27 @@ import time
 # LINEAR
 
 # n = 1000
-n = 10000
-
-
-tic = time.time()
-for i in range(n):
-    x = (i,i)
-for j in range(n):
-    x = (j,j)
-toc =time.time()
-print(toc-tic)
+# n = 10000
+#
+#
+# tic = time.time()
+# for i in range(n):
+#     x = (i,i)
+# for j in range(n):
+#     x = (j,j)
+# toc =time.time()
+# print(toc-tic)
 
 # FACTORIAL
-
-(n-1)! =  -1 (mod n)
-
-n = 1000000
-fact = 1
-tic = time.time()
-for i in range(1,n+1):
-    fact = fact * i
-toc = time.time()s
-print(toc-tic)
-print ("The factorial of 23 is : ",end="")
-print (fact)
-
-360.93027091026306
-10 ^ 6 = 11110100001001000000 in base 2, 20 cifre
+#
+# (n-1)! =  -1 (mod n)
+#
+# n = 1000000
+# fact = 1
+# tic = time.time()
+# for i in range(1,n+1):
+#     fact = fact * i
+# toc = time.time()s
+# print(toc-tic)
+# print ("The factorial of 23 is : ",end="")
+# print (fact)
